8a87e1d6a53058448f189456074fd1b6cf1b5c9a
Werner Ovalle <wernerovalle1995@hotmail.com>
2021-04-29 18:24:06 -0600

    docker v2

--- index.js
+++ index.js
@@ -7,9 +7,16 @@ var port = process.env.PORT || 3999;
 mongoose.set('useFindAndModify', false);//evitar que salga un mensaje en consola unicamente cuando se actualiza
 
 mongoose.Promise = global.Promise;
+//usar set NODE_ENV=development para cambiar
+const NODE_ENV = process.env.NODE_ENV || 'production';
+require ('dotenv').config({
+  path: `${NODE_ENV}.env`
+});
 
+var url = 'mongodb://'+process.env.MONGO_USERNAME+':'+process.env.MONGO_PASSWORD+'@'+process.env.MONGO_HOSTNAME+':'+process.env.MONGO_PORT+'/'+process.env.MONGO_DB;
+ console.log(url)
 mongoose
-  .connect("mongodb://localhost:27017/api_rest_node", { useNewUrlParser: true })
+  .connect(url, { useNewUrlParser: true })
   .then(() => {
     console.log("Connection successful");
     //CREAR EL SERVIDOR
